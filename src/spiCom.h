a159b5e6e5c1825310872797b59a3459cbf71518
ryan-1976 <2357618933@qq.com>
2018-03-05 19:08:24 +0800

    2018-03-05-03

---
+++ src/spiCom.h
@@ -0,0 +1,48 @@
+
+
+#ifndef SPICOM_H
+#define SPICOM_H
+
+#include <linux/types.h>
+typedef  unsigned char  U08;
+typedef  unsigned short U16;
+typedef  unsigned int   U32;
+typedef  signed char    I08;
+typedef  signed short   I16;
+
+typedef struct{
+	U08        startFlag;
+	U08        packetLen[2];
+	U08        packetNum[2];
+	U08        siteId[4];
+	U08        devId;
+	U08        packetCmd;
+	U08        packetAnswer;
+}COM_HEAD_TYPE;
+
+#define PACKET_HEAD_LEN (sizeof(COM_HEAD_TYPE))
+#define PACKET_CONTEXT_LEN 729
+#define PACKET_LEN (PACKET_HEAD_LEN+PACKET_CONTEXT_LEN+3)
+
+typedef union
+{
+	U08             buffer[PACKET_LEN];
+    COM_HEAD_TYPE   head;
+} COM_TYPE;
+
+typedef union
+{
+	U32   lword;
+	U08   byte[4];
+} UNION_4_BYTE;
+typedef union
+{
+	U16   word;
+	U08   byte[2];
+} UNION_2_BYTE;
+#define ARRAY_SIZE(a) (sizeof(a)/sizeof((a)[0]))
+#define BUFFER_SIZE 768
+#define SPI_DEVICE "/dev/spidev0.0"
+#define GPIO_DEVICE "/sys/class/gpio/gpio68/value"
+
+#endif /* SPICOM_H */
---
+++ src/spiFuntions.c
@@ -0,0 +1,40 @@
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <unistd.h>  
+#include "circlebuff.h"
+#include "spiCom.h"
+U08 cdtuBuf[PACKET_LEN];
+extern U08 spiRxBuff[];
+static U16 g_comPacketIdx=0;
+static U32 g_siteId=0x01020304;
+
+void spi2MqtttPacket(void)
+{
+	COM_TYPE *pCom;
+	UNION_4_BYTE tmp4;
+	UNION_2_BYTE tmp2;
+	U16 i;
+
+	pCom =(COM_TYPE *)&cdtuBuf[0];
+	pCom->head.startFlag = 0x7e;
+	tmp2.word =PACKET_LEN-2;
+	pCom->head.packetLen[1] = tmp2.byte[1];
+	pCom->head.packetLen[0] = tmp2.byte[0];
+	tmp2.word =g_comPacketIdx;
+	pCom->head.packetNum[1] = tmp2.byte[1];
+	pCom->head.packetNum[0] = tmp2.byte[0];
+	tmp4.lword= g_siteId;
+	pCom->head.siteId[3] = tmp4.byte[3];
+	pCom->head.siteId[2] = tmp4.byte[2];
+	pCom->head.siteId[1] = tmp4.byte[1];
+	pCom->head.siteId[0] = tmp4.byte[0];
+	pCom->head.devId =     spiRxBuff[1];
+	pCom->head.packetCmd = 0x82;
+	pCom->head.packetAnswer = 0xff;
+    for(i=0;i<PACKET_CONTEXT_LEN;i++){
+    	pCom->buffer[i+PACKET_HEAD_LEN]=spiRxBuff[i+1];
+    }
+    pCom->buffer[PACKET_LEN-1]=0x7e;
+	AP_circleBuff_WritePacket(cdtuBuf,PACKET_LEN,DTU2MQTPA);
+
+}
